f9ae399c5d2ea21a4f4e2cc9e683ff9c96d18699
NGUYENANH1272002 <140064272+NGUYENANH1272002@users.noreply.github.com>
2023-09-07 17:44:53 +0700

    toantu

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,12 +1,20 @@
-﻿internal class Program
-{
-    private static void Main(String[] args)
-    {
+﻿
         const int a = 10;
         int b;
       System.Console.Write("gia tri b la: " );
         b=Convert.ToInt32(Console.ReadLine());
         int c = a + b;
         System.Console.WriteLine("c=" + c);
-    }
+if (a < b)
+{
+    System.Console.WriteLine(a + "<" + b);
 }
+else if (a > b)
+{
+    System.Console.WriteLine(a + ">" + b);
+}
+else if (a == b)
+{
+    System.Console.WriteLine(a + "=" + b);
+}
+
