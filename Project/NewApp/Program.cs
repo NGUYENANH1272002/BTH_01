fa46bafba13c8a052ac04d7106e65b040f60a8ab
NGUYENANH1272002 <140064272+NGUYENANH1272002@users.noreply.github.com>
2023-09-07 17:53:46 +0700

    tinh

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,20 +1,21 @@
-﻿
-        const int a = 10;
+﻿internal class Program
+{
+    private static void Main(String[] args)
+    {
+        int a;
+        System.Console.Write("gia tri cua a la: ");
+        a = Convert.ToInt32(Console.ReadLine());
         int b;
-      System.Console.Write("gia tri b la: " );
-        b=Convert.ToInt32(Console.ReadLine());
-        int c = a + b;
-        System.Console.WriteLine("c=" + c);
-if (a < b)
-{
-    System.Console.WriteLine(a + "<" + b);
-}
-else if (a > b)
-{
-    System.Console.WriteLine(a + ">" + b);
-}
-else if (a == b)
-{
-    System.Console.WriteLine(a + "=" + b);
-}
+        System.Console.Write("gia tri cua b la: ");
+        b = Convert.ToInt32(Console.ReadLine());
+        int c = a - b;
+        int d = a * b;
+        int e = a / b;
+        int f = a % b;
+        System.Console.WriteLine("gia tri cua c= " + c);
+        System.Console.WriteLine("gia tri cua d= " + d);
+        System.Console.WriteLine("gia tri cua e= " + e);
+        System.Console.WriteLine("va phan du la: " + f);
 
+    }
+}
