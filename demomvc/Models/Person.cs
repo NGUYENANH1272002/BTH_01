321991248298327a7dfc77825c03f0fad08af20d
NGUYENANH1272002 <nguyenthivananh12072002@gmail.com>
2023-11-10 23:47:15 +0700

    slide 67

--- demomvc/Models/Person.cs
+++ demomvc/Models/Person.cs
@@ -8,6 +8,8 @@ namespace demomvc.Models{
         [Key]
         public string PersonID { get; set; }
         public string FullName { get; set; }
+        public int Age { get; set; }
          public string Address { get; set; }
+
     }
 }
